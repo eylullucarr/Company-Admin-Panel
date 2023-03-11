703275cd03a0e92dda4a7690f16aae447336897e
eylullucarr <eylullucarr@outlook.com.tr>
2023-03-11 16:42:50 +0300

    dropdown location

--- example-project/src/app/location/location.component.ts
+++ example-project/src/app/location/location.component.ts
@@ -1,9 +1,5 @@
 import { Component, OnDestroy, OnInit } from '@angular/core';
 import { ConfirmationService, MessageService } from 'primeng/api';
-import { Subscription } from 'rxjs';
-import * as FileSaver from 'file-saver';
-import jsPDF from 'jspdf';
-import autoTable from 'jspdf-autotable';
 import { CommonModule } from '@angular/common';
 import { HttpClientModule } from '@angular/common/http';
 import { TableModule } from 'primeng/table';
@@ -13,10 +9,8 @@ import { DialogModule } from 'primeng/dialog';
 import { ConfirmDialogModule } from 'primeng/confirmdialog';
 import { ToastModule } from 'primeng/toast';
 import { TooltipModule } from 'primeng/tooltip';
-
 import { DropdownModule } from 'primeng/dropdown';
 import { LocationService } from './location.service';
-import { response } from 'express';
 import { Location } from './location';
 import { FormsModule } from '@angular/forms';
 
@@ -53,8 +47,15 @@ export class LocationComponent implements OnInit {
 
   ngOnInit(): void {
     this.locationservice.getLocationList().subscribe((data) => {
-      this.location = data;
+      this.location = data; //urlden çektiği dataları location dizisine atiyor.
       this.uniqueCountries = Array.from(new Set(data.map((l) => l.country)));
+      //(data.map((l) => l.country)) datadaki her bir öğeye l diyor. sonra her l'nin county
+      //özelliğini alıp map metodu tüm objeler için tekrarlar.Yani biz burada tüm objelerin
+      //counrty bilgisini aldık. array.from ile bu verileri diziye atılır AMA buradaki
+      //new set ile benzersiz değerler sadece seçilir.
+      //(map)=>countryleri toplayarak dizi yapar
+      //(new set)=>countryleri distinctler,bir veri yapısı
+      //(array.form)=>distictlenen verilerin dizileşmesini sağlar
     });
   }
 
@@ -63,14 +64,24 @@ export class LocationComponent implements OnInit {
     const filteredLocations = this.location.filter(
       (l) => l.country === this.selectedCountry
     );
+    //yukarıda datadaki değerledi locationa atmıştık.
+    //this.location.filter diyerek bu değerlerin hepsini l ye atiyoruz. daha sonrasında
+    //l 'deki tüm objeleri  obejeler bitene kadar seçilen ülkenin ismi ile karşıalştırıyoruz
+    //true dönenler l objelerini de filteredlocations a atiyoruz
     console.log('Filtered location: ', filteredLocations);
   }
 
   getCities(selectedCountry: string): string[] {
-    return this.location
-      .filter((l) => l.country === selectedCountry)
-      .map((l) => l.city)
-      .filter((value, index, self) => self.indexOf(value) === index);
+    return (
+      this.location
+        .filter((l) => l.country === selectedCountry)
+        //location verilerini l parametresine verir. Ve l'deki objesinin country değerini
+        //selectedcountry değerine eşit mi diye kontrol eder.
+        .map((l) => l.city)
+        //filterda true dönen objelerin city değerini map ile alır ve diziye dönüştürür.
+        .filter((value, index, self) => self.indexOf(value) === index)
+    );
+    //Bu fonksiyon, her bir dizi elemanını kontrol eder ve elemanın dizideki ilk indeksini belirler (self.indexOf(value)). Eğer elemanın dizideki ilk indeksi, elemanın şu anki indeksiyle eşleşiyorsa (self.indexOf(value) === index), bu elemanın benzersiz olduğu anlamına gelir ve bu elemanı yeni dizide tutmak için true değeri döndürür. Eğer elemanın dizideki ilk indeksi, elemanın şu anki indeksiyle eşleşmiyorsa, bu elemanın daha önce dizide yer aldığı anlamına gelir ve bu elemanı yeni dizide tutmamak için false değeri döndürür.
   }
 
   onSelectionChangeCity(event: any) {
